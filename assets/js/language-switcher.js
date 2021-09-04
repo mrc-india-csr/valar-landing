ae67d5e961cbd39db3cd281694cc900ad9b4415d
karthi.chinnaraj <karthi.chinnaraj@gmail.com>
2021-09-04 14:01:59 +0530

    language-translate

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -22,7 +22,15 @@ const EN = {
     "login": "LOGIN",
     "header_line_1": "Making Tamil Nadu a trillion dollar manufacturing hub",
     "header_line_2": "The Government of Tamil Nadu welcomes all Industry 4.0 service providers, researchers, and individuals to come together, collaborate and breakthrough the industrial 4.0 revolution.",
-    "sign_up": "SIGN UP TODAY"
+    "sign_up": "SIGN UP TODAY",
+    "service_title": "Research Knowledge",
+    "service_description": "Easily find and get connected with other service providers, researchers, and subject matter experts with whom you can exchange expertise, research knowledge, services, facilities, and other resources for your business, research or infrastructure needs.",
+    "card_title_1": "Reach out for assistance",
+    "card_desc_1": "Post a problem statement or challenge you need help with, and search for existing problems / solutions in the areas of your interest",
+    "card_title_2": "Offer your expertise",
+    "card_desc_2": "Offer your services, share your past projects in the industry, research and infrastructure facilities",
+    "card_title_3": "Suggest solutions",
+    "card_desc_3": "Offer suggestions to others problem statements / challenges"
 
 }
 
@@ -42,7 +50,15 @@ const TA = {
     "login": "உள்நுழையவும்",
     "header_line_1": "தமிழகத்தை ஒரு டிரில்லியன் டாலர் உற்பத்தி மையமாக உருவாக்குவது",
     "header_line_2": "தமிழக அரசு அனைத்து தொழில் 4.0 சேவை வழங்குநர்கள், ஆராய்ச்சியாளர்கள் மற்றும் தனிநபர்களை ஆகியோர் ஒன்றிணைந்து உடனுழைக்க மற்றும் தொழில்துறை 4.0 புரட்சியை ஆக்கபூர்வமாக முன்னெடுக்க வரவேற்கிறது.",
-    "sign_up": "இன்றே பதிவு செய்யுங்கள்"
+    "sign_up": "இன்றே பதிவு செய்யுங்கள்",
+    "service_title": "உதவிக்கு அணுகவும்",
+    "service_description": "உங்களுக்கு தேவையான சேவை வழங்குநர்கள், ஆராய்ச்சியாளர்கள் மற்றும் பொருள் வல்லுநர்களுடன் இணைய மற்றும் அவர்களுடன் உங்கள் வணிகம், ஆராய்ச்சி, உள்கட்டமைப்பு தேவைகளை பூர்த்தி செய்யக்கூடிய நிபுணத்துவம், வசதிகள், ஆராய்ச்சி அறிவு, மற்றும் பிற வளங்களை பரிமாறிக்கொள்ளலாம்.",
+    "card_title_1": "உதவிக்கு அணுகவும்",
+    "card_desc_1": "நீங்கள் சந்திக்கும் பிரச்சனை அல்லது சவால் சார்ந்த தலைப்புகளை தேடலாம். மேலும் உங்களுக்கு விருப்பமான துறையில் இருக்கும் பிரச்சனைகள் அல்லது தீர்வுகளை தேடலாம்.",
+    "card_title_2": "உங்கள் நிபுணத்துவத்தை வழங்க",
+    "card_desc_2": "உங்கள் சேவைகளை வழங்குங்கி, தொழில், ஆராய்ச்சி மற்றும் உள்கட்டமைப்பு வசதிகளில் உங்கள் கடந்தகால திட்டங்களைப் பகிர்ந்து கொள்ளுங்கள்.",
+    "card_title_3": "தீர்வுகளை பரிந்துரைக்க",
+    "card_desc_3": "மற்றவர்களின் பிரச்சனைகள் / சவால்களுக்கு உங்கள் யோசனையை பரிந்துரையுங்கள்."
 }
 
 
