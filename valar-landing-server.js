e0e775b71a47d73f0e50dbbbb7d83914753ccfcc
Deepag kumar B <deepagkumar@gmail.com>
2021-09-07 17:30:01 +0530

    Added support for https

--- valar-landing-server.js
+++ valar-landing-server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const https = require('https');
+const fs = require('fs');
 
 // Create Express Server
 const app = express();
 
 // Configuration
-const PORT = 80;
-const HOST = "0.0.0.0";//TODO Change this to the host ip
+const PORT = 443;
 
 app.use('/', express.static('./'));
 //TODO update the URL to where the valar login App is deployed
 app.use('/**', createProxyMiddleware({ target: 'http://localhost:8080/', changeOrigin: true }));
 
+//Create the https server
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/home/deepag/Desktop/valar/privatekey.pem'),
+  cert: fs.readFileSync('/home/deepag/Desktop/valar/cert.pem'),
+}, app);
+
 
 // Start the Proxy
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy server at ${HOST}:${PORT}`);
+httpsServer.listen(PORT, () => {
+  console.log(`Starting Https Proxy server at port ${PORT}`);
 });
