6ec0d40efc8794d551e781040af757a203c9e31b
Deepag kumar B <deepagkumar@gmail.com>
2021-09-08 18:24:22 +0530

    updated path for keys

--- valar-landing-server.js
+++ valar-landing-server.js
@@ -20,7 +20,7 @@ app.use(function(request, response, next) {
 
 app.use('/', express.static('./'));
 //TODO update the URL to where the valar login App is deployed
-app.use('/**', createProxyMiddleware({ target: 'http://117.239.70.26/', changeOrigin: true }));
+app.use('/**', createProxyMiddleware({ target: 'http://localhost:8080/', changeOrigin: true }));
 
 //Create the http server
 const httpServer = http.createServer(app);
@@ -33,8 +33,8 @@ httpServer.listen(HTTP_PORT, () => {
 
 //Create the https server
 const httpsServer = https.createServer({
-  key: fs.readFileSync('/home/deepag/Desktop/valar/privatekey.pem'),
-  cert: fs.readFileSync('/home/deepag/Desktop/valar/cert.pem'),
+  key: fs.readFileSync('/home/appuser/SSL/key.pem'),
+  cert: fs.readFileSync('/home/appuser/SSL/server.crt'),
 }, app);
 
 const HTTPS_PORT = 443
