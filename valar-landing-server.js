8a71146369dc0ea831555934d153f471f19467af
Deepag kumar B <deepagkumar@gmail.com>
2021-09-08 11:28:03 +0530

    Added http to https redirect

--- valar-landing-server.js
+++ valar-landing-server.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const https = require('https');
-//const http = require('http');
+const http = require('http');
 const fs = require('fs');
 
 // Create Express Server
 const app = express();
 
+app.enable('trust proxy');
+
+//Redirect http to https
+app.use(function(request, response, next) {
+  if (!request.secure) {
+     return response.redirect("https://" + request.headers.host + request.url);
+  }
+
+  next();
+})
+
 app.use('/', express.static('./'));
 //TODO update the URL to where the valar login App is deployed
 app.use('/**', createProxyMiddleware({ target: 'http://localhost:8080/', changeOrigin: true }));
 
-// //Create the http server
-// const httpServer = http.createServer(app);
+//Create the http server
+const httpServer = http.createServer(app);
 
-// const HTTP_PORT = 8081
-// // Start the Proxy
-// httpServer.listen(HTTP_PORT, () => {
-//   console.log(`Starting Http Proxy server at port ${HTTP_PORT}`);
-// });
+const HTTP_PORT = 80
+// Start the Proxy
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Starting Http Proxy server at port ${HTTP_PORT}`);
+});
 
 //Create the https server
 const httpsServer = https.createServer({
-  key: fs.readFileSync('/home/deepag/Desktop/valar/privatekey.pem'),
-  cert: fs.readFileSync('/home/deepag/Desktop/valar/cert.pem'),
+  key: fs.readFileSync('<key-path>/privatekey.pem'),
+  cert: fs.readFileSync('<key-path>/cert.pem'),
 }, app);
 
 const HTTPS_PORT = 443
